56db4208c338385d6148f6eb62d01622f58980a0
Junjie <cjunjie09@gmail.com>
2025-12-04 14:07:04 +0800

    Added Error handling for division operation

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const subtract = (a, b) => a - b;
 
 const multiply = (a, b) => a * b;
 
-const divide = (a, b) => a / b;
+const divide = (a, b) => {
+    if(b === 0) return "Error";
+    return a / b;
+};
 
 console.log(add(1, 2));
 console.log(subtract(14, -10));
