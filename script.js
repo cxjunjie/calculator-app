22dfd22bc5946ae90442b3442f77df267e3049a4
Junjie <cjunjie09@gmail.com>
2025-12-04 14:12:52 +0800

    Implemented operate function for the operators

--- script.js
+++ script.js
@@ -9,7 +9,34 @@ const divide = (a, b) => {
     return a / b;
 };
 
-console.log(add(1, 2));
-console.log(subtract(14, -10));
-console.log(multiply(3, 2));
-console.log(divide(6, 2));
+// console.log(add(1, 2));
+// console.log(subtract(14, -10));
+// console.log(multiply(3, 2));
+// console.log(divide(6, 2));
+
+let firstNum = null;
+let operator = null;
+let secondNum = null;
+
+const operate = (firstNum, operator, secondNum) => {
+    switch (operator) {
+        case "+":
+            return add(firstNum, secondNum);
+
+        case "-":
+            return subtract(firstNum, secondNum);
+        
+        case "*":
+            return multiply(firstNum, secondNum);
+            
+        case "/":
+            return divide(firstNum, secondNum);
+        
+        default:
+            return "Invalid operator";
+    }
+};
+
+// console.log(operate(1, "+", 2));
+// console.log(operate(10, "/", 2));
+console.log(operate(2, "5", 2));
